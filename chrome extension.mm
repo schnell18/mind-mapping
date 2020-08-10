cf0865343e139f56a2af20435f9b92e2b29db278
Justin Zhang <schnell18@gmail.com>
2020-08-10 23:43:47 +0800

    Add Google Chrome extension knowledge

---
+++ chrome extension.mm
@@ -0,0 +1,107 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="chrome extension" FOLDED="false" ID="ID_215598447" CREATED="1597062669575" MODIFIED="1597062676391" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="intro" POSITION="right" ID="ID_1421021607" CREATED="1597062694764" MODIFIED="1597062720670">
+<edge COLOR="#ff0000"/>
+<node TEXT="small programs" ID="ID_466306330" CREATED="1597062721619" MODIFIED="1597062726175"/>
+<node TEXT="Modify DOM of web page" ID="ID_1591349662" CREATED="1597062726386" MODIFIED="1597062741838"/>
+<node TEXT="HTML, CSS &amp; JavaScript" ID="ID_1758566426" CREATED="1597062742212" MODIFIED="1597062750415"/>
+</node>
+<node TEXT="concept" POSITION="right" ID="ID_1083596106" CREATED="1597062808649" MODIFIED="1597062812769">
+<edge COLOR="#0000ff"/>
+<node TEXT="browser action" ID="ID_1101121666" CREATED="1597062813633" MODIFIED="1597062819504">
+<node TEXT="toolbar" ID="ID_1167977392" CREATED="1597063069862" MODIFIED="1597063083568"/>
+</node>
+<node TEXT="page action" ID="ID_1428760423" CREATED="1597062819813" MODIFIED="1597062822453">
+<node TEXT="web page" ID="ID_1252411530" CREATED="1597063085376" MODIFIED="1597063089893"/>
+</node>
+<node TEXT="background" ID="ID_60626334" CREATED="1597063062483" MODIFIED="1597063066916"/>
+</node>
+<node TEXT=".crx file" POSITION="left" ID="ID_705532942" CREATED="1597062912982" MODIFIED="1597062951786">
+<edge COLOR="#00ff00"/>
+<node TEXT="manifest" ID="ID_1460044355" CREATED="1597062918173" MODIFIED="1597062923150">
+<node TEXT="JSON format" ID="ID_488310079" CREATED="1597063141718" MODIFIED="1597063146575"/>
+<node TEXT="fields" ID="ID_1491907801" CREATED="1597063148700" MODIFIED="1597063156680">
+<node TEXT="Manifest Version" ID="ID_541693501" CREATED="1597063157819" MODIFIED="1597063166776"/>
+<node TEXT="Name of the ext" ID="ID_103020176" CREATED="1597063167185" MODIFIED="1597063173050"/>
+<node TEXT="Version of the ext" ID="ID_1612321507" CREATED="1597063173262" MODIFIED="1597063176582"/>
+</node>
+</node>
+<node TEXT="HTML" ID="ID_250661971" CREATED="1597062923481" MODIFIED="1597062925040"/>
+<node TEXT="CSS" ID="ID_585155193" CREATED="1597062925263" MODIFIED="1597062926911"/>
+<node TEXT="JavaScript" ID="ID_1186297599" CREATED="1597062927780" MODIFIED="1597062932032"/>
+</node>
+<node TEXT="pages" POSITION="left" ID="ID_1549314243" CREATED="1597071614600" MODIFIED="1597071617839">
+<edge COLOR="#ff00ff"/>
+<node TEXT="background page" ID="ID_1251443564" CREATED="1597071619748" MODIFIED="1597071624100">
+<node TEXT="always runs" ID="ID_565220480" CREATED="1597071677246" MODIFIED="1597071685472"/>
+</node>
+<node TEXT="event page" ID="ID_102217110" CREATED="1597071624327" MODIFIED="1597071627006">
+<node TEXT="only required" ID="ID_329171156" CREATED="1597071688586" MODIFIED="1597071694517"/>
+</node>
+</node>
+</node>
+</map>
